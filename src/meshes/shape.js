8257bdfdd72401d405255fb3587cd45ce6f4693f
simplay <michael.single@students.unibe.ch>
2014-03-17 00:52:09 +0100

    Initial work for definiting a mesh container object called shape
    each shape contains all mesh data, a shader, a material.

---
+++ src/meshes/shape.js
@@ -0,0 +1,33 @@
+
+var Shape = function() {
+  var prototype.shader = "";
+  var prototype.material = "";
+  var prototype.vertices = "";
+  var prototype.faces = "";
+  
+  
+  return{
+    setShader: function(shader) {
+      this.prototype.shader = shader;
+      return this;
+    },
+
+    setFaces: function(faces) {
+      this.prototype.faces = faces;
+      return this;
+    },
+
+    setIndices: function(indices) {
+      this.prototype.indices = indices;
+      return this;
+    },
+    
+    setIndices: function(indices) {
+      this.prototype.indices = indices;
+      return this;
+    }
+       
+  }
+}();
+
+
